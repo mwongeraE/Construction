4a2f1c18389365863ea041c7b42da6c90a2f7816
Mwongera Evans <evansmwongera@gmail.com>
2019-11-01 15:38:54 +0300

    svgo install

---
+++ .cache/async-requires.js
@@ -0,0 +1,15 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---src-templates-single-project-js": () => import("../src/templates/singleProject.js" /* webpackChunkName: "component---src-templates-single-project-js" */),
+  "component---src-pages-404-js": () => import("../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-about-js": () => import("../src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
+  "component---src-pages-careers-js": () => import("../src/pages/careers.js" /* webpackChunkName: "component---src-pages-careers-js" */),
+  "component---src-pages-contact-js": () => import("../src/pages/contact.js" /* webpackChunkName: "component---src-pages-contact-js" */),
+  "component---src-pages-index-js": () => import("../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-page-2-js": () => import("../src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */),
+  "component---src-pages-projects-js": () => import("../src/pages/projects.js" /* webpackChunkName: "component---src-pages-projects-js" */),
+  "component---src-pages-services-js": () => import("../src/pages/services.js" /* webpackChunkName: "component---src-pages-services-js" */)
+}
+
---
+++ .cache/sync-requires.js
@@ -0,0 +1,18 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---src-templates-single-project-js": hot(preferDefault(require("/home/evans/Documents/Construction/src/templates/singleProject.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/home/evans/Documents/Construction/src/pages/404.js"))),
+  "component---src-pages-about-js": hot(preferDefault(require("/home/evans/Documents/Construction/src/pages/about.js"))),
+  "component---src-pages-careers-js": hot(preferDefault(require("/home/evans/Documents/Construction/src/pages/careers.js"))),
+  "component---src-pages-contact-js": hot(preferDefault(require("/home/evans/Documents/Construction/src/pages/contact.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/home/evans/Documents/Construction/src/pages/index.js"))),
+  "component---src-pages-page-2-js": hot(preferDefault(require("/home/evans/Documents/Construction/src/pages/page-2.js"))),
+  "component---src-pages-projects-js": hot(preferDefault(require("/home/evans/Documents/Construction/src/pages/projects.js"))),
+  "component---src-pages-services-js": hot(preferDefault(require("/home/evans/Documents/Construction/src/pages/services.js")))
+}
+
